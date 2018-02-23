f249a0918f640d6eceebfb800802f2c617d1ae2a
kylesallette <kylesallette@gmail.com>
2018-02-23 10:01:13 -0700

    final commit

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,12 @@
 class Trip < ApplicationRecord
 has_many :trails
 
+def average_length
+ average(:length)
+end
+
+def total_length
+ sum(:length)
+end
 
 end
--- spec/features/user_sees_trip_index_clicks_link_to_trip_sees_trails_spec.rb
+++ spec/features/user_sees_trip_index_clicks_link_to_trip_sees_trails_spec.rb
@@ -1,17 +1,31 @@
 require 'rails_helper'
 
   describe 'user sees trip index page'  do
-    it 'user clicks trip link and sees trails for that trip'  do
+    it 'user sees all trips'  do
        trip = Trip.create(name: "Super fun")
        trail = trip.trails.create(length: 234, name: "big bend", address: "1234 Jacksonville, FL" )
 
        visit trips_path
 
-      click_link("Super fun")
 
-      expect(curent_path).to eq trip_path(trip)
 
-      
+      expect(page).to have_content("Super fun")
 
-     end
+  end
+end
+
+
+ describe 'user sees trip index page'  do
+   it 'user clicks trip link and sees trails for that trip'  do
+     trip = Trip.create(name: "Super fun")
+     trail = trip.trails.create(length: 234, name: "big bend", address: "1234 Jacksonville, FL" )
+
+     visit trips_path
+
+    click_link("Super fun")
+
+    expect(current_path).to eq trip_path(trip)
+    expect(page).to have_content("big bend")
+    
+  end
 end
